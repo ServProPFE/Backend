8ba9a4bdd5d2575cba1c78ea4fa58c15c6a58b87
hechmi10 <hechmi.jaidane@gmail.com>
2026-02-17 16:06:22 +0100

    Fixed transaction bug

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const { authenticate, authorizeRoles } = require("../middleware/auth");
 //Importer les fonctions du contrôleur
-const { listTransactions, createTransaction,updateTransaction,deleteTransaction } = require("../controllers/transactionsController");
+const {
+	listTransactions,
+	createTransaction,
+	updateTransactionStatus,
+	deleteTransaction,
+} = require("../controllers/transactionController");
 //Créer un routeur Express
 const router = express.Router();
 //Définir les routes pour les transactions
 router.get("/", authenticate, authorizeRoles("ADMIN"), listTransactions);
 router.post("/", authenticate, authorizeRoles("ADMIN"), createTransaction);
-router.put("/:id", authenticate, authorizeRoles("ADMIN"), updateTransaction);
+router.put("/:id", authenticate, authorizeRoles("ADMIN"), updateTransactionStatus);
 router.delete("/:id", authenticate, authorizeRoles("ADMIN"), deleteTransaction);
 //Exporter le routeur
 module.exports = router;
